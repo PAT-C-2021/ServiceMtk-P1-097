3dfb93396d579a35b3042a560f373814b66d6e44
ffadelm <faysalfadelm0107@gmail.com>
2021-10-15 21:53:12 +0700

    Add project files.

---
+++ IMatematika.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20190140097
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        /*output*/
+        int Tambah(int a, int b); //method
+        [OperationContract]
+        /*output*/
+        int Kurang(int a, int b); //input
+        [OperationContract]
+        /*output*/
+        int Kali(int a, int b); 
+        [OperationContract]
+        /*output*/
+        int Bagi(int a, int b);
+        [OperationContract]
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //obj dari class
+    }
+    [DataContract]
+    public class Koordinat
+    {
+        private int _x, _y; //attribut
+        [DataMember] // membuka koor x,y
+        public int X //properties
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public int Y //properties
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+    }
+
+}
---
+++ Matematika.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20190140097
+{
+    public class Matematika : IMatematika
+    {
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+        public int Kali(int a, int b)
+        {
+            return a * b;
+        }
+        public int Bagi(int a, int b)
+        {
+            return a / b;
+        }
+
+        /*TKoordinat:nama methodnya a,b = input seperti int.
+         membuat koordinat hasil lalu menjumlahkann xa dengan xb, ya dengan yb lalu dimasukkan ke hasil.*/
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            Koordinat hasil = new Koordinat();
+            hasil.X = a.X + b.X;
+            hasil.Y = a.Y + b.Y;
+            return hasil;
+        }
+    }
+}
